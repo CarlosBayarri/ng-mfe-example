9c37fbeea76d628a85c8a6b75917aa0119bbdd05
Carlos Bayarri Cebrecos <e_cbayar@mercadona.com>
2021-09-07 12:27:07 +0200

    routing changes

--- src/app/mfe1/mfe1-routing.module.ts
+++ src/app/mfe1/mfe1-routing.module.ts
@@ -5,13 +5,19 @@ import { Mfe1Component } from './mfe1.component';
 const routes: Routes = [
 
   { path: '', component: Mfe1Component, children: [
+
     {
-      path: 'products',
-      loadChildren: () => import('./products/products.module').then(m => m.ProductsModule)
+      path: '',
+      redirectTo: 'home',
+      pathMatch: 'full'
     },
     {
       path: 'home',
       loadChildren: () => import('./home/home.module').then(m => m.HomeModule)
+    },
+    {
+      path: 'products',
+      loadChildren: () => import('./products/products.module').then(m => m.ProductsModule)
     },
     {
       path: 'about',
